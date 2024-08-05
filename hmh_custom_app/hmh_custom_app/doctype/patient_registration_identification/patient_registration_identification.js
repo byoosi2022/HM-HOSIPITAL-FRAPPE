ad47431cd52b8d2627e5853d736a82c0e040c340
byoosi2022 <byoosi2021@gmail.com>
2024-08-05 20:19:57 +0300

    updates

--- hmh_custom_app/hmh_custom_app/doctype/patient_registration_identification/patient_registration_identification.js
+++ hmh_custom_app/hmh_custom_app/doctype/patient_registration_identification/patient_registration_identification.js
@@ -1,8 +1,12 @@
 // Copyright (c) 2024, Paul Mututa and contributors
 // For license information, please see license.txt
-
 // frappe.ui.form.on("Patient Registration Identification", {
-// 	refresh(frm) {
-
-// 	},
+//     onload: function(frm) {
+//         // Ensure the custom phone field is initialized with Uganda code
+//         if (!frm.doc.patient_phone || !frm.doc.patient_phone.startsWith('+256')) {
+//             frm.set_value('patient_phone', '+256');
+//         }
+//     }
 // });
+
+
