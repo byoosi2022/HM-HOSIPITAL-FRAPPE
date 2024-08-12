560b4ef5f7d72d01058892344a59c30c5cbaeb6c
byoosi2022 <byoosi2021@gmail.com>
2024-08-12 17:19:34 +0300

    working on the prescriptions

--- hmh_custom_app/doctor_jouney_prescription/encounter_insurance.py
+++ hmh_custom_app/doctor_jouney_prescription/encounter_insurance.py
@@ -34,16 +34,20 @@ def update_drug_payment_status(doc, method):
         updated = False
 
         # Access the child table 'drug Prescription'
-        for drugs in patient_encounter.drug_prescription:
+        for drug in patient_encounter.drug_prescription:
             # Check if the drug is already Fully Paid
-            if drugs.custom_drug_status != "Fully Paid" and patient_doc.customer_group == "Insurance":
+            if drug.custom_drug_status != "Fully Paid" and patient_doc.customer_group == "Insurance":
                 # Check if any invoice related to this encounter has no outstanding amount
                 matching_invoices = [invoice for invoice in invoices if invoice.custom_patient_ecounter_id == encounter.name]
                 if all(invoice.outstanding_amount <= 0 for invoice in matching_invoices):
-                    drugs.custom_drug_status = "Fully Paid"
+                    drug.custom_drug_status = "Fully Paid"
                     updated = True
-                    investigations.append(drugs)
-              
+                    investigations.append(drug)
+                    
+                    # Create a new Pharmacy document
+                    create_pharmacy(drug, patient_encounter.patient, 
+                                    patient_encounter.name,patient_encounter.encounter_date,
+                                    patient_encounter.practitioner)
 
         if updated:
             patient_encounter.save()
@@ -52,4 +56,38 @@ def update_drug_payment_status(doc, method):
 
     return investigations
 
+def create_pharmacy(drug, patient, encounter_id,date,practitioner):
+    # Fetch the Patient document to get gender 
+    patient_doc = frappe.get_doc('Patient', patient)
+    patient_reg = frappe.get_doc('Patient Registration Identification', patient.custom_patient_mrno)
+    
+    # Logging for debugging
+    frappe.log_error(f"Creating create_pharmacy for Patient: {patient}, Encounter ID: {encounter_id}", "create_pharmacy Creation")
+
+    # Create a new Pharmacy document 
+    pharmacy_doc = frappe.new_doc('Pharmacy')
+    pharmacy_doc.patient = patient
+    pharmacy_doc.patient_sex = patient_doc.sex
+    pharmacy_doc.patient_age = patient_reg.age
+    pharmacy_doc.encounter_date = date
+    pharmacy_doc.practitioner = practitioner
+    pharmacy_doc.patient_encounter_id = encounter_id  
+    pharmacy_doc.medical_department = patient_doc.custom_consulting_department  
+    # Append item to the Sales Invoice 
+    pharmacy_doc.append("drug_prescription", {
+        "drug_code": drug.drug_code,
+        "dosage": drug.dosage,
+        "period": drug.period,
+        "qty": drug.custom_rate,
+        "dosage_form": drug.dosage_form,
+        "strength": drug.strength,
+        "strength_uom": drug.strength_uom,
+
+     
+    })
+    # Logging for debugging
+    frappe.log_error(f"Lab Test Doc Values: {pharmacy_doc.as_dict()}", "Lab Test Doc Debug")
+
+    pharmacy_doc.insert()
+    frappe.db.commit()  # Commit changes to the database
    
